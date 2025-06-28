793fac325f8017e87842bbc4ff13176a2554712f
MUHAMMAD ZAHIRUL <jibonsarker228@gmail.com>
2025-06-28 14:01:13 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 javascript
 function handleDownload() {
   alert("অ্যাপ ডাউনলোড লিংক খুব শীঘ্রই যুক্ত হবে। আপাতত ধন্যবাদ!");
-  // window.location.href = "https://yourdownloadlink.com"; // Uncomment this line when you have the real link
+  // window.location.href = "https://yourdownloadlink.com";
 }
